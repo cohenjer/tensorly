a1ce5309f8e61962cb4f0188e461d927440a85a0
COHEN Jeremy <jeremy.cohen@cnrs.fr>
2024-05-06 15:14:20 +0200

    Adding TTV and TTM support in numpy core tenalg through mode_dot

--- tensorly/plugins.py
+++ tensorly/plugins.py
@@ -1,14 +1,98 @@
 import tensorly as tl
+from tensorly.base import unfold, fold, vec_to_tensor
 
 # Author: Jean Kossaifi
 
 
 PREVIOUS_EINSUM = None
+PREVIOUS_MODE_DOT = None
 OPT_EINSUM_PATH_CACHE = dict()
 CUQUANTUM_PATH_CACHE = dict()
 CUQUANTUM_HANDLE = None
 
 
+def use_ttm_bassoy():
+    """Swap the default tensorly implementation of mode_dot (which relies on the backend) for the Cem Bassoy TTM implementation in C, available [here](https://github.com/bassoy/ttm/tree/main)."""
+    global PREVIOUS_MODE_DOT
+    
+    try:
+        import ttmpy
+        import ttvpy
+    except ImportError as error:
+        message = (
+            "Impossible to import ttmpy. \n"
+            "First install it:\n"
+            "todo"
+        )
+        raise ImportError(message) from error
+    
+    def bassoy_ttm(tensor, matrix_or_vector, mode, transpose=False):
+        """ Is this faster? todo"""
+        # the mode along which to fold might decrease if we take product with a vector
+        fold_mode = mode
+        new_shape = list(tensor.shape)
+
+        if tl.ndim(matrix_or_vector) == 2:  # Tensor times matrix
+            # Test for the validity of the operation
+            dim = 0 if transpose else 1
+            if matrix_or_vector.shape[dim] != tensor.shape[mode]:
+                raise ValueError(
+                    f"shapes {tensor.shape} and {matrix_or_vector.shape} not aligned in mode-{mode} multiplication: "
+                    f"{tensor.shape[mode]} (mode {mode}) != {matrix_or_vector.shape[dim]} (dim 1 of matrix)"
+                )
+
+            if transpose:
+                matrix_or_vector = tl.conj(tl.transpose(matrix_or_vector))
+
+            new_shape[mode] = matrix_or_vector.shape[0]
+            vec = False
+
+        elif tl.ndim(matrix_or_vector) == 1:  # Tensor times vector
+            if matrix_or_vector.shape[0] != tensor.shape[mode]:
+                raise ValueError(
+                    f"shapes {tensor.shape} and {matrix_or_vector.shape} not aligned for mode-{mode} multiplication: "
+                    f"{tensor.shape[mode]} (mode {mode}) != {matrix_or_vector.shape[0]} (vector size)"
+                )
+            if len(new_shape) > 1:
+                new_shape.pop(mode)
+            else:
+                new_shape = ()
+            vec = True
+
+        else:
+            raise ValueError(
+                "Can only take n_mode_product with a vector or a matrix."
+                f"Provided array of dimension {tl.ndim(matrix_or_vector)} not in [1, 2]."
+            )
+            
+        #res = tl.dot(matrix_or_vector, unfold(tensor, mode))
+        if vec:
+            print("Using TTV Bassoy")
+            res = ttvpy.ttv(mode+1, tensor, matrix_or_vector)
+            return vec_to_tensor(res, shape=new_shape)
+        else:
+            print("Using TTM Bassoy")
+            res = ttmpy.ttm(mode+1, tensor, matrix_or_vector)
+            return fold(res, fold_mode, new_shape)
+        
+    if PREVIOUS_MODE_DOT is None:
+        PREVIOUS_MODE_DOT = tl.tenalg.core_tenalg.mode_dot
+        
+    print("Switching to Bassoy mode_dot")
+    tl.tenalg.register_backend_method("mode_dot", bassoy_ttm)
+    tl.tenalg.use_dynamic_dispatch()
+
+
+def use_default_mode_dot():
+    """Revert to the original einsum for the current backend"""
+    global PREVIOUS_MODE_DOT
+
+    if PREVIOUS_MODE_DOT is not None:
+        print("Switching back to default mode_dot")
+        tl.tenalg.register_backend_method("mode_dot", PREVIOUS_MODE_DOT)
+        tl.tenalg.use_dynamic_dispatch()
+        PREVIOUS_MODE_DOT = None
+        
 def use_default_einsum():
     """Revert to the original einsum for the current backend"""
     global PREVIOUS_EINSUM
